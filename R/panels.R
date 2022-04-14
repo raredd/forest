b726e9355f9151d51fd1007822fbd366a56dda4a
Robert Redd <raredd01@gmail.com>
2022-04-14 01:18:33 -0400

    panel_ci - fix arrow warning... again

--- R/panels.R
+++ R/panels.R
@@ -142,7 +142,9 @@ panel_ci <- function(data, y = rev(seq.int(nrow(data))),
            
            varrows <- Vectorize(arrows, c('code', 'angle'))
            for (ii in seq_along(y)) {
-             len <- grconvertX(hi[ii] - lo[ii], 'user', 'in') ## zero-length warning
+             ## zero-length warning
+             len <- grconvertX(hi[ii], 'user', 'in') -
+               grconvertX(lo[ii], 'user', 'in')
              # if (anyNA(data[ii, -1L]))
              if (is.na(data[ii, 2L]) || len < 1e-3)
                next
